936ee224de21cf03a103ce63b9c9c6673a58b31c
JustinBale55 <justinbale55@hotmail.com>
2023-01-11 14:48:02 -0700

    Class setup

---
+++ NhlSolution/NhlSystemClassLibrary/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystemClassLibrary
+{
+    internal class Team
+    {
+        string _name; //not blank, contain only letters a-z
+        string _city; //not blank, must contain at least 3 or more characters
+        string _arena; //not blank
+        Conference _conference; 
+        Division _division;
+
+        //Greedy constructor
+    }
+}
